b5c8a9f51e15d46cd2417490ac4deb7814271fa8
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2017-06-14 19:19:45 -0300

    fixed tests

--- src/components/map/map.component.test.js
+++ src/components/map/map.component.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import configureStore from '../../store/store';
 
 import Map from './map.component';
 
+const store = configureStore();
+
 it('default render map component', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Map className="map" />, div);
+    ReactDOM.render(<Provider store={store}><Map className="map" /></Provider>, div);
 
     expect(div).toMatchSnapshot();
 });
